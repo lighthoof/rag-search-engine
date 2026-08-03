26b843bd17f15af26179568c26208aa879911f92
lighthoof <andrew.kuligin@gmail.com>
2026-08-03 21:40:37 +0300

    implemented primitive substring hit search

---
+++ cli/keyword_search_cli.py
@@ -0,0 +1,33 @@
+import argparse
+import json
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Keyword Search CLI")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    search_parser = subparsers.add_parser("search", help="Search movies using keywords")
+    search_parser.add_argument("query", type=str, help="Search query")
+
+    args = parser.parse_args()
+
+    with open("data/movies.json", "r") as file:
+        data = json.load(file)
+
+    results = []
+    for movie in data["movies"]:
+        if args.query in movie["title"] and len(results) < 5:
+            results.append(movie)
+
+    match args.command:
+        case "search":
+            print(f"Searching for: {args.query}")
+            for i, result in enumerate(results):
+                print(f"{i+1}. {result["title"]}")
+            
+        case _:
+            parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
